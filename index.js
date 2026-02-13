9e7c436365dc06a5ae8c95293e9df9a8c80a5147
ngowthami <ngowthami86@gmail.com>
2026-02-13 14:03:21 +0530

    added message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.json({"message": "CI/CD is awesome"})
+    res.json({"message": "CI/CD is awesome!"})
 })
 
 
